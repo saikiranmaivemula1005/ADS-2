d9778480a90cfecaf0fddcd960243c9f90fbcf3f
saikiranmaivemula1005 <saikiranmaivemula1005@msitprogram.net>
2018-10-29 21:01:35 +0530

    "ads2_m1_a1_1: 7/8 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- assignments/Module 1/m1/1.1 Graph/SequentialSearchST.java
+++ assignments/Module 1/m1/1.1 Graph/SequentialSearchST.java
@@ -1,42 +1,42 @@
-
 /**
- *  The {@code SequentialSearchST} class represents an (unordered)
- *  symbol table of generic key-value pairs.
- *  It supports the usual <em>put</em>, <em>get</em>, <em>contains</em>,
- *  <em>delete</em>, <em>size</em>, and <em>is-empty</em> methods.
- *  It also provides a <em>keys</em> method for iterating over all of the keys.
- *  A symbol table implements the <em>associative array</em> abstraction:
- *  when associating a value with a key that is already in the symbol table,
- *  the convention is to replace the old value with the new value.
- *  The class also uses the convention that values cannot be {@code null}. Setting the
- *  value associated with a key to {@code null} is equivalent to deleting the key
- *  from the symbol table.
- *  <p>
- *  This implementation uses a singly-linked list and sequential search.
- *  It relies on the {@code equals()} method to test whether two keys
- *  are equal. It does not call either the {@code compareTo()} or
- *  {@code hashCode()} method. 
- *  The <em>put</em> and <em>delete</em> operations take linear time; the
- *  <em>get</em> and <em>contains</em> operations takes linear time in the worst case.
- *  The <em>size</em>, and <em>is-empty</em> operations take constant time.
- *  Construction takes constant time.
- *  <p>
- *  For additional documentation, see <a href="https://algs4.cs.princeton.edu/31elementary">Section 3.1</a> of
- *  <i>Algorithms, 4th Edition</i> by Robert Sedgewick and Kevin Wayne.
+ * Class for sequential search st.
  *
- *  @author Robert Sedgewick
- *  @author Kevin Wayne
+ * @param      <Key>    The key
+ * @param      <Value>  The value
  */
 public class SequentialSearchST<Key, Value> {
-    private int n;           // number of key-value pairs
+    /**
+     * { var_description }
+     */
+    private int n;
+    /**
+     * { item_description }
+     */// number of key-value pairs
     private Node first;      // the linked list of key-value pairs
 
-    // a helper linked list data type
+/**
+ * Class for node.
+ */
     private class Node {
+        /**
+         * { var_description }
+         */
         private Key key;
+        /**
+         * { var_description }
+         */
         private Value val;
+        /**
+         * { var_description }
+         */
         private Node next;
-
+        /**
+         * Constructs the object.
+         *
+         * @param      key   The key
+         * @param      val   The value
+         * @param      next  The next
+         */
         public Node(Key key, Value val, Node next)  {
             this.key  = key;
             this.val  = val;
--- assignments/Module 1/m1/1.1 Graph/Solution.java
+++ assignments/Module 1/m1/1.1 Graph/Solution.java
@@ -1,82 +1,158 @@
 import java.util.Scanner;
+/**
+ * Class for graph.
+ */
 class Graph {
+	/**
+	 * integer variable vertices.
+	 */
 	private  int vertices;
-    private int edges;
-    private Bag<Integer>[] adj;
-    Graph() {
+	/**
+	 * integer variable edges.
+	 */
+	private int edges;
+	/**
+	 * array of bag type.
+	 */
+	private Bag<Integer>[] adj;
+	/**
+	 * Constructs the object.
+	 */
+	Graph() {
 
-    }
-    Graph(int v) {
-    	this.vertices = v;
-    	this.edges = 0;
-    	adj = (Bag<Integer>[]) new Bag[v];
-    	for (int i = 0; i < v; i++) {
-            adj[i] = new Bag<Integer>();
-        }
-    }
-    public int V() {
-    	return vertices;
-    }
-    public int E() {
-    	return edges;
-    }
-    public void addEdge(int v, int w) {
-       if (!hasEdge(v, w)) {
-        edges++;
-       }
-       if (v == w) {
-       	return;
-       }
-        adj[v].add(w);
-        adj[w].add(v);
-       }
-    public boolean hasEdge(int v, int w) {
-    	if (adj[v] == null) {
-    		return true;
-    	}
-    	for (int i : adj[v]) {
-    		if (i == w) {
-    			return true;
-    		}
-    	}
-    	return false;
-    }
-    public Iterable<Integer> adj(int v) {
-        return adj[v];
-    }
-    public int degree(int v) {
-        return adj[v].size();
-    }
-    public int check(int v, int w) {
-        if (hasEdge(v, w) == true) {
-        	return 1;
-        }
-        return 0;
-    }
-    public Bag[] matrix() {
-    	return adj;
-    }
-    public Bag[] list() {
-    	return adj;
-    }
-
+	}
+	/**
+	 * Constructs the object.
+	 *
+	 * @param      v     integer variable.
+	 */
+	Graph(int v) {
+		this.vertices = v;
+		this.edges = 0;
+		adj = (Bag<Integer>[]) new Bag[v];
+		for (int i = 0; i < v; i++) {
+			adj[i] = new Bag<Integer>();
+		}
+	}
+	/**
+	 * returns vertices.
+	 *
+	 * @return  vertices.
+	 */
+	public int V() {
+		return vertices;
+	}
+	/**
+	 * returns edges.
+	 *
+	 * @return edges.
+	 */
+	public int E() {
+		return edges;
+	}
+	/**
+	 * Adds an edge.
+	 *
+	 * @param      v     integer variable.
+	 * @param      w     integer variable.
+	 */
+	public void addEdge(int v, int w) {
+		if (!hasEdge(v, w)) {
+			edges++;
+			adj[v].add(w);
+			adj[w].add(v);
+		}
+		if (v == w) {
+			return;
+		}
+	}
+	/**
+	 * Determines if it has edge.
+	 *
+	 * @param      v     integer variable.
+	 * @param      w     integer variable.
+	 *
+	 * @return     True if has edge, False otherwise.
+	 */
+	public boolean hasEdge(int v, int w) {
+		if (adj[v] == null) {
+			return true;
+		}
+		for (int i : adj[v]) {
+			if (i == w) {
+				return true;
+			}
+		}
+		return false;
+	}
+	/**
+	 * iterable function.
+	 *
+	 * @param      v integer variable.
+	 *
+	 * @return   array.
+	 */
+	public Iterable<Integer> adj(int v) {
+		return adj[v];
+	}
+	/**
+	 * to return the connection between vertices.
+	 *
+	 * @param      v    integer variable.
+	 * @param      w    integer variable.
+	 *
+	 * @return  1 if there is connection, else 0.
+	 */
+	public int check(int v, int w) {
+		if (hasEdge(v, w) == true) {
+			return 1;
+		}
+		return 0;
+	}
+	/**
+	 * matrix method.
+	 *
+	 * @return   array.
+	 */
+	public Bag[] matrix() {
+		return adj;
+	}
+	/**
+	 * list method.
+	 *
+	 * @return  array.
+	 */
+	public Bag[] list() {
+		return adj;
+	}
 }
+/**
+ * Class for solution.
+ */
 class Solution {
+	/**
+	 * Constructs the object.
+	 */
 	protected Solution() {
 
 	}
-	public static void main(String[] args) {
+	/**
+	 * main method.
+	 *
+	 * @param      args  The arguments
+	 */
+	public static void main(final String[] args) {
 		Scanner sc = new Scanner(System.in);
 		String format = sc.nextLine();
 		int vertices = Integer.parseInt(sc.nextLine());
 		int edges = Integer.parseInt(sc.nextLine());
 		Graph g = new Graph(vertices);
 		String[] inputs = sc.nextLine().split(",");
-		SequentialSearchST<Integer, String> st = new SequentialSearchST<Integer, String>();
-		for (int i = 0; i < vertices; i++) {
-			st.put(i, inputs[i]);
-		}
-		if(inputs.length > 2) {
+		// SequentialSearchST<Integer, String> st = new SequentialSearchST<Integer, String>();
+		// for (int i = 0; i < vertices; i++) {
+		// 	st.put(i, inputs[i]);
+		// }
+		if (inputs.length > 2) {
 			for (int i = 0; i < edges; i++) {
 				String[] tokens = sc.nextLine().split(" ");
 				g.addEdge(Integer.parseInt(tokens[0]), Integer.parseInt(tokens[1]));
@@ -88,33 +164,33 @@ class Solution {
 			return;
 		}
 		switch (format) {
-			case "Matrix":
-				Bag<Integer>[] adj = g.matrix();
-				int[][] matrix = new int[vertices][vertices];
-				for (int i = 0; i < vertices; i++) {
-					for (int j = 0; j < vertices; j++) {
-						if (g.hasEdge(i, j)) {
-							matrix[i][j] = 1;
-						}
+		case "Matrix":
+			Bag<Integer>[] adj = g.matrix();
+			int[][] matrix = new int[vertices][vertices];
+			for (int i = 0; i < vertices; i++) {
+				for (int j = 0; j < vertices; j++) {
+					if (g.hasEdge(i, j)) {
+						matrix[i][j] = 1;
 					}
 				}
-				for (int i = 0; i < vertices; i++) {
-					for (int j = 0; j < vertices; j++) {
-						System.out.print(matrix[i][j] + " ");
-					}
-					System.out.println();
+			}
+			for (int i = 0; i < vertices; i++) {
+				for (int j = 0; j < vertices; j++) {
+					System.out.print(matrix[i][j] + " ");
 				}
-				break;
-			case "List":
-				g.list();
-				adj = g.list();
-				for (int v = 0; v < vertices; v++) {
-            	System.out.print((st.get(v) + ": "));
-            		for (int w : adj[v]) {
-           	 			System.out.print((st.get(w) + " "));
-            }
-            System.out.print("\n");
-        }
+				System.out.println();
+			}
+			break;
+		case "List":
+			g.list();
+			adj = g.list();
+			for (int v = 0; v < vertices; v++) {
+				System.out.print((inputs[v] + ": "));
+				for (int w : adj[v]) {
+					System.out.print((inputs[w] + " "));
+				}
+				System.out.print("\n");
+			}
 		}
 	}
 }
