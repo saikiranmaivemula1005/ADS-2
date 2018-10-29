0edd3b79a1c07a791fc2a20c818661d1c1c38127
saikiranmaivemula1005 <saikiranmaivemula1005@msitprogram.net>
2018-10-29 21:09:10 +0530

    "ads2_m1_a1_1: 7/8 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- assignments/Module 1/m1/1.1 Graph/Bag.java
+++ assignments/Module 1/m1/1.1 Graph/Bag.java
@@ -8,75 +8,88 @@ import java.util.NoSuchElementException;
  */
 public class Bag<Item> implements Iterable<Item> {
 	/**
+	 * { var_description }.
+	 */
+	private int n;     // number of elements in bag
+	/**
 	 * { var_description }
 	 */
-    private int n;     // number of elements in bag
-    /**
-     * { var_description }
-     */
-    private Node first;    // beginning of bag
-    /**
-     * Class for node.
-     */
-    private class Node {
-        private Item item;
-        private Node next;
-    }
+	private Node first;    // beginning of bag
+	/**
+	 * Class for node.
+	 */
+	private class Node {
+		/**
+		 * { var_description }.
+		 */
+		private Item item;
+		/**
+		 * { var_description }.
+		 */
+		private Node next;
+	}
 
-   /**
-     * Create an empty stack.
-     */
-    public Bag() {
-        first = null;
-        n = 0;
-    }
+	/**
+	  * Create an empty stack.
+	  */
+	public Bag() {
+		first = null;
+		n = 0;
+	}
+	/**
+	 * Determines if empty.
+	 *
+	 * @return     True if empty, False otherwise.
+	 */
+	public boolean isEmpty() {
+		return first == null;
+	}
 
-   /**
-     * Is the BAG empty?
-     */
-    public boolean isEmpty() {
-        return first == null;
-    }
+	/**
+	  * Return the number of items in the bag.
+	  */
+	public int size() {
+		return n;
+	}
 
-   /**
-     * Return the number of items in the bag.
-     */
-    public int size() {
-        return n;
-    }
-
-   /**
-     * Add the item to the bag.
-     */
-    public void add(final Item item) {
-        Node oldfirst = first;
-        first = new Node();
-        first.item = item;
-        first.next = oldfirst;
-        n++;
-    }
+	/**
+	 * { function_description }
+	 *
+	 * @param      item  The item
+	 */
+	public void add(final Item item) {
+		Node oldfirst = first;
+		first = new Node();
+		first.item = item;
+		first.next = oldfirst;
+		n++;
+	}
 
 
-   /**
-     * Return an iterator that iterates over the items in the bag.
-     */
-    public Iterator<Item> iterator()  {
-        return new ListIterator();  
-    }
+	/**
+	  * Return an iterator that iterates over the items in the bag.
+	  */
+	public Iterator<Item> iterator()  {
+		return new ListIterator();
+	}
 
-    // an iterator, doesn't implement remove() since it's optional
-    private class ListIterator implements Iterator<Item> {
-        private Node current = first;
+	// an iterator, doesn't implement remove() since it's optional
+	private class ListIterator implements Iterator<Item> {
+		private Node current = first;
 
-        public boolean hasNext()  { return current != null;                     }
-        public void remove()      { throw new UnsupportedOperationException();  }
+		public boolean hasNext() {
+			return current != null;
+		}
+		public void remove() {
+			throw new UnsupportedOperationException();
+		}
 
-        public Item next() {
-            if (!hasNext()) throw new NoSuchElementException();
-            Item item = current.item;
-            current = current.next; 
-            return item;
-        }
-    }
+		public Item next() {
+			if (!hasNext()) throw new NoSuchElementException();
+			Item item = current.item;
+			current = current.next;
+			return item;
+		}
+	}
 
 }
