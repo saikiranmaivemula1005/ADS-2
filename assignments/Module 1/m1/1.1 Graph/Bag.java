208ae453f3a7ae6910bb02aa9f679d50d3ee6e05
saikiranmaivemula1005 <saikiranmaivemula1005@msitprogram.net>
2018-10-29 21:04:31 +0530

    "ads2_m1_a1_1: 7/8 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- assignments/Module 1/m1/1.1 Graph/Bag.java
+++ assignments/Module 1/m1/1.1 Graph/Bag.java
@@ -1,24 +1,10 @@
-/*************************************************************************
- *  Compilation:  javac Bag.java
- *  Execution:    java Bag < input.txt
- *
- *  A generic bag or multiset, implemented using a linked list.
- *
- *************************************************************************/
-
 import java.util.Iterator;
 import java.util.NoSuchElementException;
 
 /**
- *  The <tt>Bag</tt> class represents a bag (or multiset) of 
- *  generic items. It supports insertion and iterating over the 
- *  items in arbitrary order.
- *  <p>
- *  The <em>add</em>, <em>isEmpty</em>, and <em>size</em>  operation 
- *  take constant time. Iteration takes time proportional to the number of items.
- *  <p>
- *  For additional documentation, see <a href="http://algs4.cs.princeton.edu/13stacks">Section 1.3</a> of
- *  <i>Algorithms, 4th Edition</i> by Robert Sedgewick and Kevin Wayne.
+ * Class for bag.
+ *
+ * @param      <Item>  The item
  */
 public class Bag<Item> implements Iterable<Item> {
     private int N;         // number of elements in bag
--- assignments/Module 1/m1/1.1 Graph/Solution.java
+++ assignments/Module 1/m1/1.1 Graph/Solution.java
@@ -3,190 +3,190 @@ import java.util.Scanner;
  * Class for graph.
  */
 class Graph {
-	/**
-	 * integer variable vertices.
-	 */
-	private  int vertices;
-	/**
-	 * integer variable edges.
-	 */
-	private int edges;
-	/**
-	 * array of bag type.
-	 */
-	private Bag<Integer>[] adj;
-	/**
-	 * Constructs the object.
-	 */
-	Graph() {
+    /**
+     * integer variable vertices.
+     */
+    private  int vertices;
+    /**
+     * integer variable edges.
+     */
+    private int edges;
+    /**
+     * array of bag type.
+     */
+    private Bag<Integer>[] adj;
+    /**
+     * Constructs the object.
+     */
+    Graph() {
 
-	}
-	/**
-	 * Constructs the object.
-	 *
-	 * @param      v     integer variable.
-	 */
-	Graph(int v) {
-		this.vertices = v;
-		this.edges = 0;
-		adj = (Bag<Integer>[]) new Bag[v];
-		for (int i = 0; i < v; i++) {
-			adj[i] = new Bag<Integer>();
-		}
-	}
-	/**
-	 * returns vertices.
-	 *
-	 * @return  vertices.
-	 */
-	public int V() {
-		return vertices;
-	}
-	/**
-	 * returns edges.
-	 *
-	 * @return edges.
-	 */
-	public int E() {
-		return edges;
-	}
-	/**
-	 * Adds an edge.
-	 *
-	 * @param      v     integer variable.
-	 * @param      w     integer variable.
-	 */
-	public void addEdge(int v, int w) {
-		if (!hasEdge(v, w)) {
-			edges++;
-			adj[v].add(w);
-			adj[w].add(v);
-		}
-		if (v == w) {
-			return;
-		}
-	}
-	/**
-	 * Determines if it has edge.
-	 *
-	 * @param      v     integer variable.
-	 * @param      w     integer variable.
-	 *
-	 * @return     True if has edge, False otherwise.
-	 */
-	public boolean hasEdge(int v, int w) {
-		if (adj[v] == null) {
-			return true;
-		}
-		for (int i : adj[v]) {
-			if (i == w) {
-				return true;
-			}
-		}
-		return false;
-	}
-	/**
-	 * iterable function.
-	 *
-	 * @param      v integer variable.
-	 *
-	 * @return   array.
-	 */
-	public Iterable<Integer> adj(int v) {
-		return adj[v];
-	}
-	/**
-	 * to return the connection between vertices.
-	 *
-	 * @param      v    integer variable.
-	 * @param      w    integer variable.
-	 *
-	 * @return  1 if there is connection, else 0.
-	 */
-	public int check(int v, int w) {
-		if (hasEdge(v, w) == true) {
-			return 1;
-		}
-		return 0;
-	}
-	/**
-	 * matrix method.
-	 *
-	 * @return   array.
-	 */
-	public Bag[] matrix() {
-		return adj;
-	}
-	/**
-	 * list method.
-	 *
-	 * @return  array.
-	 */
-	public Bag[] list() {
-		return adj;
-	}
+    }
+    /**
+     * Constructs the object.
+     *
+     * @param      v     integer variable.
+     */
+    Graph(int v) {
+        this.vertices = v;
+        this.edges = 0;
+        adj = (Bag<Integer>[]) new Bag[v];
+        for (int i = 0; i < v; i++) {
+            adj[i] = new Bag<Integer>();
+        }
+    }
+    /**
+     * returns vertices.
+     *
+     * @return  vertices.
+     */
+    public int V() {
+        return vertices;
+    }
+    /**
+     * returns edges.
+     *
+     * @return edges.
+     */
+    public int E() {
+        return edges;
+    }
+    /**
+     * Adds an edge.
+     *
+     * @param      v     integer variable.
+     * @param      w     integer variable.
+     */
+    public void addEdge(int v, int w) {
+        if (!hasEdge(v, w)) {
+            edges++;
+            adj[v].add(w);
+            adj[w].add(v);
+        }
+        if (v == w) {
+            return;
+        }
+    }
+    /**
+     * Determines if it has edge.
+     *
+     * @param      v     integer variable.
+     * @param      w     integer variable.
+     *
+     * @return     True if has edge, False otherwise.
+     */
+    public boolean hasEdge(int v, int w) {
+        if (adj[v] == null) {
+            return true;
+        }
+        for (int i : adj[v]) {
+            if (i == w) {
+                return true;
+            }
+        }
+        return false;
+    }
+    /**
+     * iterable function.
+     *
+     * @param      v integer variable.
+     *
+     * @return   array.
+     */
+    public Iterable<Integer> adj(int v) {
+        return adj[v];
+    }
+    /**
+     * to return the connection between vertices.
+     *
+     * @param      v    integer variable.
+     * @param      w    integer variable.
+     *
+     * @return  1 if there is connection, else 0.
+     */
+    public int check(int v, int w) {
+        if (hasEdge(v, w) == true) {
+            return 1;
+        }
+        return 0;
+    }
+    /**
+     * matrix method.
+     *
+     * @return   array.
+     */
+    public Bag[] matrix() {
+        return adj;
+    }
+    /**
+     * list method.
+     *
+     * @return  array.
+     */
+    public Bag[] list() {
+        return adj;
+    }
 }
 /**
  * Class for solution.
  */
 class Solution {
-	/**
-	 * Constructs the object.
-	 */
-	protected Solution() {
+    /**
+     * Constructs the object.
+     */
+    protected Solution() {
 
-	}
-	/**
-	 * main method.
-	 *
-	 * @param      args  The arguments
-	 */
-	public static void main(final String[] args) {
-		Scanner sc = new Scanner(System.in);
-		String format = sc.nextLine();
-		int vertices = Integer.parseInt(sc.nextLine());
-		int edges = Integer.parseInt(sc.nextLine());
-		Graph g = new Graph(vertices);
-		String[] inputs = sc.nextLine().split(",");
-		if (inputs.length > 2) {
-			for (int i = 0; i < edges; i++) {
-				String[] tokens = sc.nextLine().split(" ");
-				g.addEdge(Integer.parseInt(tokens[0]), Integer.parseInt(tokens[1]));
-			}
-		}
-		System.out.println(g.V() + " vertices, " + g.E() + " edges");
-		if (inputs.length < 2) {
-			System.out.println("No edges");
-			return;
-		}
-		switch (format) {
-		case "Matrix":
-			Bag<Integer>[] adj = g.matrix();
-			int[][] matrix = new int[vertices][vertices];
-			for (int i = 0; i < vertices; i++) {
-				for (int j = 0; j < vertices; j++) {
-					if (g.hasEdge(i, j)) {
-						matrix[i][j] = 1;
-					}
-				}
-			}
-			for (int i = 0; i < vertices; i++) {
-				for (int j = 0; j < vertices; j++) {
-					System.out.print(matrix[i][j] + " ");
-				}
-				System.out.println();
-			}
-			break;
-		case "List":
-			g.list();
-			adj = g.list();
-			for (int v = 0; v < vertices; v++) {
-				System.out.print((inputs[v] + ": "));
-				for (int w : adj[v]) {
-					System.out.print((inputs[w] + " "));
-				}
-				System.out.print("\n");
-			}
-		}
-	}
+    }
+    /**
+     * main method.
+     *
+     * @param      args  The arguments
+     */
+    public static void main(final String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String format = sc.nextLine();
+        int vertices = Integer.parseInt(sc.nextLine());
+        int edges = Integer.parseInt(sc.nextLine());
+        Graph g = new Graph(vertices);
+        String[] inputs = sc.nextLine().split(",");
+        if (inputs.length > 2) {
+            for (int i = 0; i < edges; i++) {
+                String[] tokens = sc.nextLine().split(" ");
+                g.addEdge(Integer.parseInt(tokens[0]), Integer.parseInt(tokens[1]));
+            }
+        }
+        System.out.println(g.V() + " vertices, " + g.E() + " edges");
+        if (inputs.length < 2) {
+            System.out.println("No edges");
+            return;
+        }
+        switch (format) {
+        case "Matrix":
+            Bag<Integer>[] adj = g.matrix();
+            int[][] matrix = new int[vertices][vertices];
+            for (int i = 0; i < vertices; i++) {
+                for (int j = 0; j < vertices; j++) {
+                    if (g.hasEdge(i, j)) {
+                        matrix[i][j] = 1;
+                    }
+                }
+            }
+            for (int i = 0; i < vertices; i++) {
+                for (int j = 0; j < vertices; j++) {
+                    System.out.print(matrix[i][j] + " ");
+                }
+                System.out.println();
+            }
+            break;
+        case "List":
+            g.list();
+            adj = g.list();
+            for (int v = 0; v < vertices; v++) {
+                System.out.print((inputs[v] + ": "));
+                for (int w : adj[v]) {
+                    System.out.print((inputs[w] + " "));
+                }
+                System.out.print("\n");
+            }
+        }
+    }
 }
