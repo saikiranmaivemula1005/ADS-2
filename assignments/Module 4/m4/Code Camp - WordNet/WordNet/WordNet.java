5d97c5e280ec1ddd2e1e5658b3520d3278a162f5
saikiranmaivemula1005 <saikiranmaivemula1005@msitprogram.net>
2018-11-02 19:09:48 +0530

    "ads2_m4_a1_1: 15/15 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- assignments/Module 4/m4/Code Camp - WordNet/WordNet/WordNet.java
+++ assignments/Module 4/m4/Code Camp - WordNet/WordNet/WordNet.java
@@ -40,7 +40,7 @@ public class WordNet {
     public WordNet(final String synsets, final String hypernyms) {
         try {
             File fileOne = new File(
-                "C:\\Users\\sai kiranmai\\Documents\\ADS-2\\assignments\\Module\\m4\\Code Camp - WordNet\\WordNet\\Files" + "\\" + synsets);
+                "C:\\Users\\sai kiranmai\\Documents\\ADS-2\\assignments\\Module 4\\m4\\Code Camp - WordNet\\WordNet\\Files" + "\\" + synsets);
             Scanner fOne = new Scanner(fileOne);
             File fileTwo = new File("C:\\Users\\sai kiranmai\\Documents\\ADS-2\\assignments\\Module 4\\m4\\Code Camp - WordNet\\WordNet\\Files"
                   + "\\" + hypernyms);
