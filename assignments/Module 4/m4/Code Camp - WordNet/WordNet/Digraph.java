93959e0e7493b3654c0e77febd4972e53c5518ee
saikiranmaivemula1005 <saikiranmaivemula1005@msitprogram.net>
2018-11-02 15:04:50 +0530

    "ads2_m4_a1_1: 2/15 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- assignments/Module 4/m4/Code Camp - WordNet/WordNet/Digraph.java
+++ assignments/Module 4/m4/Code Camp - WordNet/WordNet/Digraph.java
@@ -174,22 +174,22 @@ public class Digraph {
         }
         return reverse;
     }
-    // /**
-    //  * Returns a string representation of the graph.
-    //  *
-    //  * @return the number of vertices <em>V</em>, followed by the number of edges <em>E</em>,
-    //  *         followed by the <em>V</em> adjacency lists
-    //  */
-    // public String toString() {
-    //     StringBuilder s = new StringBuilder();
-    //     s.append(V + " vertices, " + E + " edges " + NEWLINE);
-    //     for (int v = 0; v < V; v++) {
-    //         s.append(String.format("%d: ", v));
-    //         for (int w : adj[v]) {
-    //             s.append(String.format("%d ", w));
-    //         }
-    //         s.append(NEWLINE);
-    //     }
-    //     return s.toString();
-    // }
+    /**
+     * Returns a string representation of the graph.
+     *
+     * @return the number of vertices <em>V</em>, followed by the number of edges <em>E</em>,
+     *         followed by the <em>V</em> adjacency lists
+     */
+    public String toString() {
+        StringBuilder s = new StringBuilder();
+        s.append(vertices + " vertices, " + edges + " edges " + NEWLINE);
+        for (int v = 0; v < vertices; v++) {
+            s.append(String.format("%d: ", v));
+            for (int w : adj[v]) {
+                s.append(String.format("%d ", w));
+            }
+            s.append(NEWLINE);
+        }
+        return s.toString();
+    }
 }
