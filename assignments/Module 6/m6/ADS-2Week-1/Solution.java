b6189c0d543b477c3389dd764b0aea9915d1b160
saikiranmaivemula1005 <saikiranmaivemula1005@msitprogram.net>
2018-11-03 10:26:23 +0530

    "ads2_m6_a1_1: 0/12 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- assignments/Module 6/m6/ADS-2Week-1/Solution.java
+++ assignments/Module 6/m6/ADS-2Week-1/Solution.java
@@ -3,7 +3,7 @@ class PageRank {
 	Digraph dg;
 	PageRank(Digraph digraph) {
 		this.dg = digraph;
-		
+
 	}
 }
 
@@ -18,34 +18,35 @@ public class Solution {
 		Scanner sc = new Scanner(System.in);
 		int vertices = sc.nextInt();
 		Digraph dg = new Digraph(vertices);
-		for (int i = 0; i < vertices; i++) {
-			String[] tokens = sc.nextLine().split(" ");
-			dg.addEdge(Integer.parseInt(tokens[0]), Integer.parseInt(tokens[i]));
+		String[] tokens = sc.nextLine().split(" ");
+		for (int i = 1; i < tokens.length; i++) {
+			dg.addEdge(Integer.parseInt(tokens[0]),
+			           Integer.parseInt(tokens[i]));
 		}
 		PageRank pg = new PageRank(dg);
 		System.out.println(pg);
 
-		// iterate count of vertices times 
+		// iterate count of vertices times
 		// to read the adjacency list from std input
 		// and build the graph
-		
-		
+
+
 		// Create page rank object and pass the graph object to the constructor
-		
+
 		// print the page rank object
-		
+
 		// This part is only for the final test case
-		
+
 		// File path to the web content
 		String file = "WebContent.txt";
-		
+
 		// instantiate web search object
 		// and pass the page rank object and the file path to the constructor
-		
+
 		// read the search queries from std in
 		// remove the q= prefix and extract the search word
 		// pass the word to iAmFeelingLucky method of web search
 		// print the return value of iAmFeelingLucky
-		
+
 	}
 }
