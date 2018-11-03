99f75c42292d8898fa350172b3c377bc924e7df7
saikiranmaivemula1005 <saikiranmaivemula1005@msitprogram.net>
2018-11-03 11:43:09 +0530

    "ads2_m6_a1_1: 0/12 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- assignments/Module 6/m6/ADS-2Week-1/Solution.java
+++ assignments/Module 6/m6/ADS-2Week-1/Solution.java
@@ -14,7 +14,7 @@ class PageRank {
 			// for (int j = 0; j < 1000; j++) {
 				indegrees[i] = dg.indegree(i);
 				for (int k = 0; k < indegrees[i]; k++) {
-					pagerank[i] += initalpr / dg.outdegree(dg.getindegree(i));
+					pagerank[i] += initalpr / dg.outdegree(dg.getindegree(k));
 				}
 			// }
 			System.out.println(pagerank[i]);
