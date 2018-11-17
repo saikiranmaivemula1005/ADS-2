9076eda09b3a456268e7824a4bc6e39796d053cf
saikiranmaivemula1005 <saikiranmaivemula1005@msitprogram.net>
2018-11-17 12:22:18 +0530

    "ads2_m18_a1_1: 2/5 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- assignments/Module 18/m18/ADS-2Exam-3/Solution.java
+++ assignments/Module 18/m18/ADS-2Exam-3/Solution.java
@@ -145,7 +145,7 @@ class T9 {
 			arraylist.add(tst.get(str));
 		}
 		Collections.sort(arraylist);
-		for (int i = arraylist.size(); i >k; i--) {
+		for (int i = arraylist.size(); i <k; i--) {
 			System.out.println(hmap.get(arraylist.get(i))+ " " +arraylist.get(i));
 		}
 
