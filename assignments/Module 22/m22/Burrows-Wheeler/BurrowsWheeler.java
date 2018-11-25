4115005ff0a7c6c997d21570b12aef21cbd461a7
saikiranmaivemula1005 <saikiranmaivemula1005@msitprogram.net>
2018-11-25 15:22:54 +0530

    burrows wheeler

---
+++ assignments/Module 22/m22/Burrows-Wheeler/BurrowsWheeler.java
@@ -0,0 +1,16 @@
+
+
+public class BurrowsWheeler {
+
+
+    public static void transform() {
+
+    }
+
+    public static void inverseTransform() {
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
---
+++ assignments/Module 22/m22/Burrows-Wheeler/MoveToFront.java
@@ -0,0 +1,15 @@
+
+public class MoveToFront {
+
+    public static void encode() {
+
+    }
+
+    public static void decode() {
+
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
